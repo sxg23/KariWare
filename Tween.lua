036cb01edf7b24eb6fccd96ec5e00fb0d9756a25
sxg23 <g23@disroot.org>
2025-07-02 16:46:35 +0000

    Update Tween.lua

--- Tween.lua
+++ Tween.lua
@@ -1,6 +1,6 @@
 -- custom tween
 
-local Signal = loadstring(game:HttpGet("https://raw.githubusercontent.com/vozoid/utility/main/Signal.lua"))()
+local Signal = loadstring(game:HttpGet("https://raw.githubusercontent.com/sxg23/KariWare/main/Signal.lua"))()
 
 local render = game:GetService("RunService").RenderStepped
 local sqrt, sin, pi, halfpi, doublepi = math.sqrt, math.sin, math.pi, math.pi / 2, math.pi * 2
